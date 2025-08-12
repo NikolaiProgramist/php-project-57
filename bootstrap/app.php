c4735a3b4c14b1a55e43e861a7aaa932d2630b8a
NikolaiProgramist <nikluk2005@mail.ru>
2025-08-12 13:39:49 +0300

    Remove unused parameter $exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -11,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
