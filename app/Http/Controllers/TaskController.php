ed69b59953ed6ad9c745085834a43d952eec1100
NikolaiProgramist <nikluk2005@mail.ru>
2025-09-17 18:03:47 +0300

    Add Task, TaskController, TaskPolicy, TaskFactory, TaskSeeder, task/index.blade.php, route in the web.php

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use Illuminate\View\View;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('task.index', ['tasks' => Task::paginate(10)]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
