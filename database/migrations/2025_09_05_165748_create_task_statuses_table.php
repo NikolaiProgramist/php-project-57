9bef529fa546b2c894a759c96dc7151ca6082105
NikolaiProgramist <nikluk2005@mail.ru>
2025-09-19 19:27:37 +0300

    Change column type from text to string and add unique index

--- database/migrations/2025_09_05_165748_create_task_statuses_table.php
+++ database/migrations/2025_09_05_165748_create_task_statuses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('task_statuses', function (Blueprint $table) {
             $table->id();
-            $table->text('name')->isNotEmpty();
+            $table->string('name')->unique()->isNotEmpty();
             $table->timestamps();
         });
     }
