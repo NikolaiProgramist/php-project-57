2ecb69ef06dc7ab3532791ed716a7aaf0d674daa
NikolaiProgramist <nikluk2005@mail.ru>
2025-09-22 20:54:15 +0300

    Fix spaces for button to the register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,9 +48,7 @@
             </a>
 
             <button type="submit"
-                    class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
-                Зарегистрировать
-            </button>
+                    class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">{{ __('Зарегистрировать') }}</button>
         </div>
     </form>
 </x-guest-layout>
