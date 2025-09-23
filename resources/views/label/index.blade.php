26fc38bd63c8e6a294765a8109d83d5afe6b9bc9
NikolaiProgramist <nikluk2005@mail.ru>
2025-09-23 17:27:47 +0300

    Fix button when user as guest for hexlet-check to the label/index.blade.php

--- resources/views/label/index.blade.php
+++ resources/views/label/index.blade.php
@@ -6,8 +6,10 @@
     <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
         <div class="grid col-span-full">
             <div>
-                <a href="{{ route('labels.create') }}"
-                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Создать метку</a>
+                @auth
+                    <a href="{{ route('labels.create') }}"
+                       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Создать метку</a>
+                @endauth
             </div>
 
             <table class="mt-4">
