a0b3876416037c9790309e8e5e89f8c2b82c676a
NikolaiProgramist <nikluk2005@mail.ru>
2025-09-26 12:20:05 +0300

    Remove unused parameters to the TaskStatusPolicy.php

--- app/Policies/TaskStatusPolicy.php
+++ app/Policies/TaskStatusPolicy.php
@@ -2,7 +2,6 @@
 
 namespace App\Policies;
 
-use App\Models\TaskStatus;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,7 +18,7 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(): bool
     {
         return Auth::check();
     }
@@ -27,7 +26,7 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, TaskStatus $taskStatus): bool
+    public function update(): bool
     {
         return Auth::check();
     }
@@ -35,7 +34,7 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, TaskStatus $taskStatus): bool
+    public function delete(): bool
     {
         return Auth::check();
     }
